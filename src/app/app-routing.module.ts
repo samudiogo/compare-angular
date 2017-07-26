efead40cd8f9ebf3d84358532fb130ca74376864
Supremo Samuel Diogo <samudiogo@gmail.com>
2017-07-26 01:15:30 -0300

    initial commit, creating project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
+import { FaqComponent } from './faq/faq.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {
     path: '',
-    children: []
+    component: HomeComponent
+  },
+  {
+    path: 'faq',
+    component: FaqComponent
   }
 ];
 
